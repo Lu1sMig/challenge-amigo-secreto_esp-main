d465dd81b7ab26a500099265b823840ea6b2f58e
Lu1sM1g <lmcavero.a1@gmail.com>
2025-02-13 20:29:54 -0500

    Carga del Destok

---
+++ app.js
@@ -0,0 +1,6 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+let numeroAmigos = 4;
+let intentos = 0;
+let listaNombreAmigos = [];
+//let numeroMaximo = 10;
